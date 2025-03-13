6d65381851bddf9439c7bf33b3a5f942d860377e
Bryan Austin Suharta <66191560+BryanAustinS@users.noreply.github.com>
2025-03-13 21:56:46 +0100

    addtoCart feature created

--- ecommerce_vue/src/App.vue
+++ ecommerce_vue/src/App.vue
@@ -20,7 +20,7 @@
               
               <router-link to="/cart" class="button is-success">
                 <span class="icon"><i class="fas fa-shopping-cart"></i></span>
-                <span>Cart</span>
+                <span>Cart ({{ cartTotalLength }})</span>
               </router-link>
             </div>
           </div>
@@ -42,7 +42,25 @@
 export default{
   data(){
     return{
-      showMobileMenu: false
+      showMobileMenu: false,
+      cart: {
+        items: []
+      }
+    }
+  },
+  beforeCreate(){
+    this.$store.commit('initializeStore')
+  },
+  mounted(){
+    this.cart = this.$store.state.cart
+  },
+  computed: {
+    cartTotalLength(){
+      let totalLength = 0
+      for(let i = 0; i < this.cart.items.length; i++){
+        totalLength += this.cart.items[i].quantity
+      }
+      return totalLength
     }
   }
 }
