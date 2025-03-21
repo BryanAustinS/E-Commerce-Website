9d1566a5bd037d7857abdb3ca525687446eca6aa
Bryan Austin Suharta <66191560+BryanAustinS@users.noreply.github.com>
2025-03-21 17:33:31 +0100

    header and footer done

--- ecommerce_vue/src/App.vue
+++ ecommerce_vue/src/App.vue
@@ -1,57 +1,43 @@
 <template>
-  <div id="wrapper">
-    <nav class="navbar is-dark">
-      <div class="navbar-brand">
-        <router-link to="/" class="navbar-item"><strong>Ecommerce</strong></router-link>
-        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-
+  <div id="wrapper" class="has-background-white">
+    <nav class="navbar has-background-white">
       <div class="navbar-menu" id="navbar-menu" v-bind:class="{ 'is-active': showMobileMenu }">
-        <div class="navbar-start">
-          <div class="navbar-item">
+        <div class="navbar-brand">
+          <router-link to="/summer" class="navbar-item">Summer</router-link>
+          <router-link to="/winter" class="navbar-item">Winter</router-link>
+        </div>
+
+        <div class="navbar-center">
+          <router-link to="/" class="navbar-item"><strong>Die Mode</strong></router-link>
+        </div>
+
+        <div class="navbar-end">
+          <div class="search-container">
             <form methods="get" action="/search">
               <div class="field has-addons">
                 <div class="control">
-                  <input type="text" class="input" placeholder="What are you looking for?" name="query">
+                  <input type="text" class="input search-input" placeholder="Search" name="query">
                 </div>
-
                 <div class="control">
-                  <button type="submit" class="button is-success">
+                  <button type="submit" class="button search-button">
                     <span class="icon">
                       <i class="fas fa-search"></i>
                     </span>
                   </button>
                 </div>
-
               </div>
             </form>
           </div>
-        </div>
-        
-        <div class="navbar-end">
-          <router-link to="/summer" class="navbar-item">Summer</router-link>
-          <router-link to="/winter" class="navbar-item">Winter</router-link>
-
-          <div class="navbar-item">
-            <div class="buttons">
-              <template v-if="$store.state.isAuthenticated">
-                <router-link to="/my-account" class="button is-light">My account</router-link>
-              </template>
-
-              <template v-else>
-                <router-link to="/log-in" class="button is-light">Log in</router-link>
-              </template>
-              
-              <router-link to="/cart" class="button is-success">
-                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
-                <span>Cart ({{ cartTotalLength }})</span>
-              </router-link>
-            </div>
-          </div>
+          <template v-if="$store.state.isAuthenticated">
+            <router-link to="/my-account" class="navbar-item is-white">My account</router-link>
+          </template>
+          <template v-else>
+            <router-link to="/log-in" class="navbar-item is-white">Log in</router-link>
+          </template>
+          <router-link to="/cart" class="navbar-item is-white">
+            <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+            <span>Cart ({{ cartTotalLength }})</span>
+          </router-link>
         </div>
       </div>
     </nav>
@@ -64,41 +50,56 @@
       <router-view/>
     </section>
 
-    <footer class="footer">
-      <p class="has-text-centered">Copyright (c) 2025</p>
+    <footer class="footer has-background-black">
+      <div class="content has-text-centered">
+        <p>
+          <a href="">
+            <svg class="svg-inline--fa fa-facebook-square fa-w-14 fa-2x" aria-hidden="true" data-prefix="fab" data-icon="facebook-square" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="white" d="M448 80v352c0 26.5-21.5 48-48 48h-85.3V302.8h60.6l8.7-67.6h-69.3V192c0-19.6 5.4-32.9 33.5-32.9H384V98.7c-6.2-.8-27.4-2.7-52.2-2.7-51.6 0-87 31.5-87 89.4v49.9H184v67.6h60.9V480H48c-26.5 0-48-21.5-48-48V80c0-26.5 21.5-48 48-48h352c26.5 0 48 21.5 48 48z"></path></svg><!-- <i class="fab fa-facebook-square fa-2x"></i> -->
+          </a>  
+          <a href="">
+            <svg class="svg-inline--fa fa-twitter-square fa-w-14 fa-2x" aria-hidden="true" data-prefix="fab" data-icon="twitter-square" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="white" d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-48.9 158.8c.2 2.8.2 5.7.2 8.5 0 86.7-66 186.6-186.6 186.6-37.2 0-71.7-10.8-100.7-29.4 5.3.6 10.4.8 15.8.8 30.7 0 58.9-10.4 81.4-28-28.8-.6-53-19.5-61.3-45.5 10.1 1.5 19.2 1.5 29.6-1.2-30-6.1-52.5-32.5-52.5-64.4v-.8c8.7 4.9 18.9 7.9 29.6 8.3a65.447 65.447 0 0 1-29.2-54.6c0-12.2 3.2-23.4 8.9-33.1 32.3 39.8 80.8 65.8 135.2 68.6-9.3-44.5 24-80.6 64-80.6 18.9 0 35.9 7.9 47.9 20.7 14.8-2.8 29-8.3 41.6-15.8-4.9 15.2-15.2 28-28.8 36.1 13.2-1.4 26-5.1 37.8-10.2-8.9 13.1-20.1 24.7-32.9 34z"></path></svg><!-- <i class="fab fa-twitter-square fa-2x"></i> -->
+          </a>
+          <a href="">
+            <svg class="svg-inline--fa fa-instagram fa-w-14 fa-2x" aria-hidden="true" data-prefix="fab" data-icon="instagram" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="white" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path></svg><!-- <i class="fab fa-instagram fa-2x"></i> -->
+          </a>
+          <a href="">
+            <svg class="svg-inline--fa fa-snapchat fa-w-16 fa-2x" aria-hidden="true" data-prefix="fab" data-icon="snapchat" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" data-fa-i2svg=""><path fill="white" d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm169.5 338.9c-3.5 8.1-18.1 14-44.8 18.2-1.4 1.9-2.5 9.8-4.3 15.9-1.1 3.7-3.7 5.9-8.1 5.9h-.2c-6.2 0-12.8-2.9-25.8-2.9-17.6 0-23.7 4-37.4 13.7-14.5 10.3-28.4 19.1-49.2 18.2-21 1.6-38.6-11.2-48.5-18.2-13.8-9.7-19.8-13.7-37.4-13.7-12.5 0-20.4 3.1-25.8 3.1-5.4 0-7.5-3.3-8.3-6-1.8-6.1-2.9-14.1-4.3-16-13.8-2.1-44.8-7.5-45.5-21.4-.2-3.6 2.3-6.8 5.9-7.4 46.3-7.6 67.1-55.1 68-57.1 0-.1.1-.2.2-.3 2.5-5 3-9.2 1.6-12.5-3.4-7.9-17.9-10.7-24-13.2-15.8-6.2-18-13.4-17-18.3 1.6-8.5 14.4-13.8 21.9-10.3 5.9 2.8 11.2 4.2 15.7 4.2 3.3 0 5.5-.8 6.6-1.4-1.4-23.9-4.7-58 3.8-77.1C183.1 100 230.7 96 244.7 96c.6 0 6.1-.1 6.7-.1 34.7 0 68 17.8 84.3 54.3 8.5 19.1 5.2 53.1 3.8 77.1 1.1.6 2.9 1.3 5.7 1.4 4.3-.2 9.2-1.6 14.7-4.2 4-1.9 9.6-1.6 13.6 0 6.3 2.3 10.3 6.8 10.4 11.9.1 6.5-5.7 12.1-17.2 16.6-1.4.6-3.1 1.1-4.9 1.7-6.5 2.1-16.4 5.2-19 11.5-1.4 3.3-.8 7.5 1.6 12.5.1.1.1.2.2.3.9 2 21.7 49.5 68 57.1 4 1 7.1 5.5 4.9 10.8z"></path></svg><!-- <i class="fab fa-snapchat fa-2x"></i> -->
+          </a>        
+        </p>
+      </div>
     </footer>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
-export default{
-  data(){
-    return{
+export default {
+  data() {
+    return {
       showMobileMenu: false,
       cart: {
         items: []
       }
     }
   },
-  beforeCreate(){
+  beforeCreate() {
     this.$store.commit('initializeStore')
 
     const token = this.$store.state.token
 
-    if(token){
+    if (token) {
       axios.defaults.headers.common['Authorization'] = `Token  ${token}`
     } else {
       axios.defaults.headers.common['Authorization'] = ""
     }
   },
-  mounted(){
+  mounted() {
     this.cart = this.$store.state.cart
   },
   computed: {
-    cartTotalLength(){
+    cartTotalLength() {
       let totalLength = 0
-      for(let i = 0; i < this.cart.items.length; i++){
+      for (let i = 0; i < this.cart.items.length; i++) {
         totalLength += this.cart.items[i].quantity
       }
       return totalLength
@@ -110,13 +111,13 @@ export default{
 <style lang="scss">
 @import '../node_modules/bulma';
 
-.lds-dual-ring{
+.lds-dual-ring {
   display: inline-block;
   width: 80px;
   height: 80px;
 }
 
-.lds-dual-ring:after{
+.lds-dual-ring:after {
   content: " ";
   display: block;
   width: 64px;
@@ -128,7 +129,7 @@ export default{
   animation: lds-dual-ring 1.2s linear infinite;
 }
 
-@keyframes lds-dual-ring{
+@keyframes lds-dual-ring {
   0% {
     transform: rotate(0deg);
   }
@@ -147,5 +148,95 @@ export default{
   &.is-loading {
     height: 80px;
   }
+}
+
+.navbar-burger span {
+  background-color: black !important;
+  border: none;
+}
+
+.navbar-center {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+.navbar-center .navbar-item strong {
+  color: black !important;
+  font-size: 1.5rem;
+}
+
+.navbar-brand .navbar-item,
+.navbar-end .navbar-item {
+  position: relative;
+  color: black !important;
+}
+
+.navbar-brand .navbar-item::after,
+.navbar-end .navbar-item::after {
+  content: "";
+  position: absolute;
+  bottom: 0px;
+  left: 0;
+  width: 0;
+  height: 2px;
+  background-color: black;
+  transition: width 0.3s ease-in-out;
+}
+
+.navbar-brand .navbar-item:hover::after,
+.navbar-end .navbar-item:hover::after {
+  width: 100%;
+}
+
+.navbar-item form {
+  position: relative;
+}
+
+.search-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.search-button {
+  background-color: white !important;
+  color: #333 !important;
+  border: 1px solid #ddd;
+}
+
+.search-input {
+  background-color: white !important; 
+  border: 1px solid #ddd;
+
+  &::placeholder {
+    color: #333;
+  }
+}
+
+.router-link-active {
+  background-color: transparent !important;
+  color: inherit !important;
+  box-shadow: none !important; 
+}
+
+.navbar-item:hover {
+  background-color: transparent !important; 
+}
+
+.navbar-menu {
+  border-bottom: black 1px solid;
+}
+
+.footer svg {
+  width: 24px;
+  height: 24px; 
+  fill: white !important; 
+}
+
+.footer a {
+  margin: 0 10px;
 }
 </style>
